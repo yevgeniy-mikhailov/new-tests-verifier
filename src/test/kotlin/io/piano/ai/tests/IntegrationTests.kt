1850efc2f4940343b145a1fa87deb2f0203eea01
Evgeniy Mikhailov <evgeniy.mikhailov@piano.io>
2020-11-15 22:12:58 +0300

    Move logic to build.gradle

---
+++ src/test/kotlin/io/piano/ai/tests/IntegrationTests.kt
@@ -0,0 +1,23 @@
+package io.piano.ai.tests
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class IntegrationTests {
+
+    @Test
+    fun testOne() = Assertions.assertTrue(true)
+
+    @Test
+    fun testTwo() = Assertions.assertTrue(false)
+
+    @Test
+    fun testThree() = Assertions.assertTrue(true)
+
+    @Test
+    fun testFour() = Assertions.assertTrue(false)
+
+    @Test
+    fun testFive() = Assertions.assertTrue(false)
+}
